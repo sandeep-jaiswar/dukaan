beff8a853e16e77cfc3c1dfbd06f53180ebb3c57
sandeep jaiswar <jaiswarsandeep119@gmail.com>
2021-04-16 16:04:30 +0530

    initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },+698//,=- `1AZlkjh7yhb 
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+  },
+};
